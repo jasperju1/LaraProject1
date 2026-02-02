f870365c82144e80efdabe3aabaf7ff887277596
jasperju <jasperjakob18@gmail.com>
2026-02-02 10:59:45 +0200

    02.02.26 1x

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Console\Commands\TimetableNotification;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command(TimetableNotification::class)->everyMinute();
+    })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
